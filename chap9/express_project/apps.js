af662eb7fff9aaa9fa5005ca0d9649d0e7a639df
Masahiro Araki <araki@kit.ac.jp>
2024-07-11 16:54:30 +0900

    fix

--- chap9/express_project/apps.js
+++ chap9/express_project/apps.js
@@ -5,13 +5,13 @@ app.set("view engine", "ejs");
 const axios = require('axios');
 const url = 'https://www.jma.go.jp/bosai/forecast/data/overview_forecast/260000.json';
 const OpenAI = require('openai');
-require('dotenv').config();
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY';  // ここに取得した API キーを記入
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 // コメント生成用プロンプト
-const prompt_for_comment ="以下の気象予報を踏まえて、兄のことが大好きな若い妹の文体で、今日を元気に過ごすためのポジティブなコメントを作成してください。\n\n ## 気象予報:\n\n" 
+const prompt_for_comment ="以下の気象予報を踏まえて、ポジティブなコメントを作成してください。\n\n ## 気象予報:\n\n" 
 // 画像生成用プロンプト
-const prompt_for_image ="以下のコメントに対応する画像を生成してください。画像中の妹は日本人の若い女性で、画像にはコメント中のキーワードを表すイメージをできるだけ多く含めてください。ただし、文字は入れないでください。\n\n## コメント\n\n"
+const prompt_for_image ="以下のコメントに対応する画像を生成してください。\n\n## コメント\n\n"
 
 app.get('/', async (req, res) => {
   try {
